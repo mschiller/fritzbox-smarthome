d8af3f73b6177e2ffc66648bc999d5830ee5ffec
Michael <michael@agil-verwirklichen.de>
2021-01-23 16:01:41 +0100

    more robust Actor::all

--- lib/fritzbox/smarthome/actor.rb
+++ lib/fritzbox/smarthome/actor.rb
@@ -18,7 +18,7 @@ module Fritzbox
           xml = nori.parse(response.body)
           Array.wrap(types.map { |type| xml.dig('devicelist', type) }.flatten).compact.map do |data|
             klass = Actor.descendants.find { |k| k.match?(data) }
-            self.in?([klass, Actor]) ? klass.new_from_api(data) : nil
+            self.in?([klass, Actor]) && klass ? klass.new_from_api(data) : nil
           end.compact
         end
 
